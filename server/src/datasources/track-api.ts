b2a3fad14e3362e5231bd946020e795bdd278038
Alexander-Gabor <gaboralexanderalexgmail.com>
2024-04-04 23:31:15 +0200

    Defining methods in TrackAPI class in the backend

--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -1,1 +1,13 @@
 import { RESTDataSource } from '@apollo/datasource-rest';
+
+export class TrackAPI extends RESTDataSource {
+    baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+    
+    getTracksForHome() {
+        return this.get('tracks');
+    }
+
+    getAuthour(authorId: string) {
+        return this.get(`authour/${authorId}`)
+    }
+}

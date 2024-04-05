1f72bb1859c3f7d74f74e62ebd69425d0f25c14b
Alexander-Gabor <gaboralexanderalexgmail.com>
2024-04-05 15:33:07 +0200

    Implementing query resolvers in the server

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -29,13 +29,15 @@ const TRACKS = gql(`
 
 const Tracks = () => {
   const { loading, error, data} = useQuery(TRACKS);
-  return <Layout grid>
-    <QueryResult error={error} loading={loading} data={data}>
-      {data?.tracksForHome?.map((track) => (
-        <TrackCard key={track.id} track={track} />
-      ))}
-    </QueryResult>
-    </Layout>;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+    )
 };
 
 export default Tracks;
--- server/src/datasources/track-api.ts
+++ server/src/datasources/track-api.ts
@@ -2,12 +2,12 @@ import { RESTDataSource } from '@apollo/datasource-rest';
 
 export class TrackAPI extends RESTDataSource {
     baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
-    
+  
     getTracksForHome() {
-        return this.get('tracks');
+      return this.get("tracks");
     }
-
-    getAuthour(authorId: string) {
-        return this.get(`authour/${authorId}`)
+  
+    getAuthor(authorId: string) {
+      return this.get(`author/${authorId}`);
     }
-}
+  }
